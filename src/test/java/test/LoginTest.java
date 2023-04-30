607d26f689e29a8ae94b91d44b14193003372edd
techfios-git <shihabul.kh@gmail.com>
2023-04-30 12:36:09 -0500

    exl project

---
+++ src/test/java/test/LoginTest.java
@@ -0,0 +1,37 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+import page.DashboardPage;
+import page.LoginPage;
+import util.BrowserFactory;
+import util.ExcelReader;
+
+public class LoginTest {
+	
+	WebDriver driver;
+	
+	// Test/Mock Data
+	ExcelReader exlRead = new ExcelReader("testData\\TF_TestData.xlsx");
+	
+	String userName = exlRead.getCellData("LoginInfo", "UserName", 2);
+	String password = exlRead.getCellData("LoginInfo", "Password", 2);
+	
+	@Test
+	public void userShouldBeAbleToLogin() {
+		
+		driver = BrowserFactory.init();
+		
+		LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
+		loginPage.insertUserName(userName);
+		loginPage.insertPassword(password);
+		loginPage.clickOnSigninButton();
+		DashboardPage dashboard = PageFactory.initElements(driver, DashboardPage.class);
+		dashboard.validateDashboardPage();
+		
+		BrowserFactory.tearDown();
+	}
+
+}
